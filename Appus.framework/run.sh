db1f13351ccad5599ff72aa9ada746cd9b94df05
ilya.borshchov.appus <ilya.borshchov@appus.me>
2015-11-05 12:01:17 +0200

    Changed source url

--- Appus.framework/run.sh
+++
@@ -1,5 +0,0 @@
-chmod 755 $PROJECT_DIR/Libraries/Appus.framework/PBCrypt
-SETTINGSPLIST=$PROJECT_DIR/resources/settings.plist
-OUTDIR=$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH
-ENC_KEY=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$INFOPLIST_FILE")
-"$PROJECT_DIR/Libraries/Appus.framework/PBCrypt" -e -k "$ENC_KEY" -i "$SETTINGSPLIST" -o "$OUTDIR/`basename $SETTINGSPLIST`"
